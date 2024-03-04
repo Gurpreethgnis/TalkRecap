21689f33150cfb9b04f9fa26d25743d3537ec8e0
Gurpreet Singh <gurpreets.nanda@gmail.com>
2024-03-04 12:16:06 -0500

    update Mar 4

--- utils.py
+++ utils.py
@@ -8,6 +8,31 @@ from pydiar.util.misc import optimize_segments
 from pydiar.models import BinaryKeyDiarizationModel, Segment
 from transformers import BartForConditionalGeneration, BartTokenizer
 
+def initialize_parameters(summary_length = 250, file_name = 'Sample_1', audio_folder = os.path.join(os.getcwd(),'audio_files')):
+    if not os.path.exists(audio_folder):
+        os.makedirs(audio_folder)
+    
+    file_path = os.path.join(audio_folder, file_name+'.wav')
+    model, tokenizer = loadSummarizer ()
+    
+    return summary_length, file_path, audio_folder, model, tokenizer
+
+def get_audio_file(file_path):
+    if not os.path.exists(file_path):
+        # Record the audio file
+        print ("\nStarting to record the audio at "+str(file_path))
+        audio_data = record_audio_to_file(file_path)
+    else:
+        #read audio file
+        print ("\nAudio file already exists. Reading the file at "+str(file_path))
+        audio_data = read_audio_file(file_path)
+    return audio_data
+
+def read_audio_file(file_path):
+    audio_data, _ = sf.read(file_path)
+    audio_data = audio_data.astype(np.float32)  # Ensure audio data is float32
+    return audio_data
+
 def record_audio_to_file(file_path, sampling_rate=16000):
     """
     Records audio from the microphone until stopped by the user and saves it to a file.
@@ -30,18 +55,11 @@ def record_audio_to_file(file_path, sampling_rate=16000):
     sf.write(file_path, audio_data, samplerate=16000)
     return audio_data
 
-
-
 def loadSummarizer(model_name = "bart-large-cnn"):
     # Load model and tokenizer
     tokenizer = BartTokenizer.from_pretrained(model_name)
     model = BartForConditionalGeneration.from_pretrained(model_name)
     return model,tokenizer
-
-def read_audio_file(file_path):
-    audio_data, _ = sf.read(file_path)
-    audio_data = audio_data.astype(np.float32)  # Ensure audio data is float32
-    return audio_data
 
 def perform_diarization(file_path):
     # Perform speaker diarization
@@ -52,7 +70,7 @@ def perform_diarization(file_path):
         segment.end = np.float32(getattr(segment, 'end', segment.start))
     return segments
 
-def transcribe_audio_with_diarization(audio_data, segments, sampling_rate=16000, model = whisper.load_model("base")):
+def transcribe_audio(audio_data, segments, sampling_rate=16000, model = whisper.load_model("base")):
     """
     Transcribes audio data using the specified Whisper model, considering speaker diarization segments.
 
@@ -112,24 +130,26 @@ def chunk_article(article, tokenizer, max_length):
         chunks.append(tokenizer.convert_tokens_to_ids(current_chunk))
     return chunks
 
-def summarize_chunks(chunks, model, tokenizer, max_length = 130, length_penalty = 2.0):
+def summarize_chunks(chunks, model, tokenizer, summary_percent = 10):
     summaries = []
     for chunk in chunks:
         input_ids = torch.tensor([chunk])
+        max_length = np.int32(len(chunk) * summary_percent * 0.01) 
+        if max_length < 30:
+            max_length = 35
         summary_ids = model.generate(input_ids, num_beams=4, max_length=max_length, min_length=30, length_penalty=2.0, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         summaries.append(summary)
     return summaries
 
-def summarize_speaker(model, transcribed_dict, tokenizer, summary_length = 250):
+def summarize_speaker(model, transcribed_dict, tokenizer, summary_percent = 10):
     summary_dict={}
     for key in transcribed_dict:
         #print (key)
         # Chunk the article
         chunks = chunk_article(''.join(transcribed_dict[key]), tokenizer, max_length=1024 - tokenizer.num_special_tokens_to_add())
-        
         # Summarize each chunk
-        summaries = summarize_chunks(chunks, model, tokenizer, max_length = summary_length)
+        summaries = summarize_chunks(chunks, model, tokenizer, summary_percent = summary_percent)
         
         # Option to further summarize the summaries or combine them
         final_summary = " ".join(summaries)
@@ -138,12 +158,13 @@ def summarize_speaker(model, transcribed_dict, tokenizer, summary_length = 250):
         #print(final_summary)
     return summary_dict
 
-def get_audio_file(file_path):
-    if not os.path.exists(file_path):
-        # Record the audio file
-        print ("\nStarting to record the audio at "+str(file_path))
-        audio_data = record_audio_to_file(file_path)
-    else:
-        #read audio file
-        print ("\nAudio file already exists. Reading the file at "+str(file_path))
-        audio_data = read_audio_file(file_path)
+def print_results(transcribed_dict, summarized_dict):
+    for key in transcribed_dict:
+        print ("\n"+"_"*50)
+        print ("Speaker: "+key)
+        print ("_"*50)
+        print ("### Transcribed Text ###\n")
+        print ('\n'.join(transcribed_dict[key]))
+        print ("."*50)
+        print ("### Summarized Text ###\n")
+        print (summarized_dict[key])
